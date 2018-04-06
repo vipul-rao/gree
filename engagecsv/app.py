0687e8fe95eb460aa694024a23235c96907395fa
vipul-rao <35137216+vipul-rao@users.noreply.github.com>
2018-04-06 13:29:10 +0530

    Update app.py

--- engagecsv/app.py
+++ engagecsv/app.py
@@ -102,8 +102,8 @@ def upload():
         n = saved_column.get(i)
         i = i+1
         ns = len(n.split())
-	if ns > 4:
-		n = 'never get a domain' 
+        if ns > 4:
+            n = 'never get a domain' 
         data = clearbit.NameToDomain.find(name=n)
         if data != None:
             res.append(data['domain'])
